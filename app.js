c4905117109e961ef284e0a6d3b762ca22a8788f
juanT-27 <jtsoftdeveloper@gmail.com>
2024-06-11 20:15:51 -0500

    Advance logic of calendar

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-// necesitamos primero invocar los elementos html necesarios
-// saber cual es el primer día de ese mes y el ultimo día
-let $calendar = document.querySelector("#calendar"),
+
+let $calendar = document.querySelector("#form-calendar"),
   $monthTitle = document.querySelector(".monthTitle"),
-  $yearTitle = document.querySelector(".yearTitle");
+  $yearTitle = document.querySelector(".yearTitle"),
+  $calendarDays= document.querySelector(".calendar-dayColumns")
 
 const months = [
   "January",
@@ -18,32 +18,87 @@ const months = [
   "November",
   "December",
 ];
+
 const changeDate = (month, year) => {
-  let date = new Date();
-  date.setMonth(month);
-  date.setFullYear(year);
-
-  let firstDayOfMonth= date.getDay(1)
-  let lastDate= new Date(year, month +1, 0).getDate()
-  let lastDay= new Date(year, month, lastDate).getDay()
+  let date = new Date(year, month);
+  console.log(date);
+  let firstDayOfMonth = date.getDay();
+  let lastDate = new Date(year, month + 1, 0).getDate();
+  let lastDay = new Date(year, month, lastDate).getDay();
   $monthTitle.textContent = months[month];
-  $yearTitle.textContent= year
+  $yearTitle.textContent = year;
 
-  setDaysInCalendar(firstDayOfMonth, lastDate, lastDay)
+  setDaysInCalendar(firstDayOfMonth, lastDate, lastDay);
 };
 
-const setDaysInCalendar=(firstDay, lastDate, lastDay)=>{
-    for(let i=1; i<lastDate;i++){
-        console.log(i)
-    }
+const setDaysInCalendar = (firstDay, lastDate, lastDay) => {
+$calendarDays.innerHTML=""
+  let days = [
+    {
+      day: "Mon",
+      number: [],
+    },
+    {
+      day: "Tuey",
+      number: [],
+    },
+    {
+      day: "Wed",
+      number: [],
+    },
+    {
+      day: "Thu",
+      number: [],
+    },
+    {
+      day: "Fri",
+      number: [],
+    },
+    {
+      day: "Sat",
+      number: [],
+    },
+    {
+      day: "Sun",
+      number: [],
+    },
+  ];
+  let adjustedFirstDay= (firstDay+6)%7;
+  let nextDay= adjustedFirstDay;
+  let currentDate = 1;
 
-    
+  while ( currentDate <= lastDate) {
 
+    days[nextDay].number.push(currentDate);
+    nextDay = (nextDay + 1) % 7;
+    currentDate++;
+
+  }
+  
+  days.forEach(day=>{
+    $calendarDays.innerHTML+= renderDays(day)
+  })
+  
+};
+
+const renderDays= (day)=>{
+let datesHtml= day.number.map(num=>`<div>${num}</div>`).join("")
+return `
+       <div class="column" id=${day.day}>
+                <div >${day.day}</div>
+                <div >${datesHtml}</div>
+            </div>`
 }
 
-$calendar.addEventListener("submit", (e) => {
+
+document.addEventListener("DOMContentLoaded", (e)=>{
+  
+  $calendar.addEventListener("submit", (e) => {
   e.preventDefault();
-  let $month = document.querySelector("#monthList").value;
-  let $year = document.querySelector("#yearInput").value;
+  let $month = parseInt(document.querySelector("#monthList").value);
+  let $year = parseInt(document.querySelector("#yearInput").value);
   changeDate($month, $year);
 });
+
+})
+
